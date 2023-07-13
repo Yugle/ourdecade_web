7884de87ddbf7d19b5429396a9cc757d8e670560
Zhu Guangyu <guangyu.zhu2@cn.bosch.com>
2023-07-13 14:09:59 +0800

    feat: update prettier rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,6 +3,9 @@ module.exports = {
   proseWrap: 'never',
   singleQuote: true,
   trailingComma: 'all',
+  tabWidth: 4,
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-packagejson'],
+  semi: false,
   overrides: [
     {
       files: '*.md',
