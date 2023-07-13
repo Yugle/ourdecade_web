3e66e639b683b0eb61de2938380281177abe9eb7
Zhu Guangyu <guangyu.zhu2@cn.bosch.com>
2023-07-13 14:18:04 +0800

    feat: replace self-closing tags

--- .dumi/theme/builtins/Covers/index.tsx
+++ .dumi/theme/builtins/Covers/index.tsx
@@ -5,7 +5,7 @@ const Covers: FC = () => {
     return (
         <>
             <h2 className='center-title'>往期封面</h2>
-            <img className='covers' src="/covers.webp"/>
+            <img className='covers' src="/covers.webp"></img>
         </>
     )
 }
