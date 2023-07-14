68f99dae6ec6eb20e066c0dc23ccb75ff3a02ed9
Zhu Guangyu <guangyu.zhu2@cn.bosch.com>
2023-07-14 09:27:14 +0800

    feat: set the title divider by antd

--- .dumi/theme/builtins/Covers/index.tsx
+++ .dumi/theme/builtins/Covers/index.tsx
@@ -1,11 +1,14 @@
+import { Divider } from 'antd'
 import React, { type FC } from 'react'
 import './index.less'
 
 const Covers: FC = () => {
     return (
         <>
-            <h2 className='center-title'>往期封面</h2>
-            <img className='covers' src="/covers.webp"></img>
+            <Divider className="title-divider">
+                <h2>往期封面</h2>
+            </Divider>
+            <img className="covers" src="/covers.webp"></img>
         </>
     )
 }
