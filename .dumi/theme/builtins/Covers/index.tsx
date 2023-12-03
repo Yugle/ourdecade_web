a00a126fb4a539d0f88485e921d939326601f934
Zhu Guangyu <guangyu.zhu2@cn.bosch.com>
2023-12-03 22:08:05 +0800

    feat: make covers clickable

--- .dumi/theme/builtins/Covers/index.tsx
+++ .dumi/theme/builtins/Covers/index.tsx
@@ -1,14 +1,50 @@
 import { Divider } from 'antd'
 import React, { type FC } from 'react'
+import { history } from 'umi'
 import './index.less'
 
+interface RequireContext {
+    keys(): string[]
+    <T>(id: string): T
+    <T>(id: string, recursive: boolean): T
+    resolve(id: string): string
+}
+
+declare const require: {
+    context: (
+        directory: string,
+        useSubdirectories: boolean,
+        regExp: RegExp,
+    ) => RequireContext
+    (id: string): any
+    resolve(id: string): string
+}
+
 const Covers: FC = () => {
+    // 获取/covers下所有图片文件
+    const imageContext = require.context(
+        '/public/covers',
+        false,
+        /\.(png|jpe?g|svg|webp)$/,
+    )
+    const imageNames = imageContext.keys()
+
     return (
         <>
             <Divider className="title-divider">
                 <h2>往期封面</h2>
             </Divider>
-            <img className="covers" src="/covers.webp"></img>
+            <div className="covers">
+                {imageNames.map((imageName) => (
+                    <img
+                        key={imageName}
+                        src={imageContext(imageName)}
+                        onClick={() => {
+                            history.push(imageName.split('.')[1])
+                        }}
+                    ></img>
+                ))}
+            </div>
         </>
     )
 }
